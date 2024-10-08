60910bc91a084d316d2adceb294d5207a4bc5a20
gouni <gounimahesh9000@gmail.com>
2024-10-08 15:09:45 +0530

    hii

--- src/main/java/com/neoteric/jenkins/jenkindemo/Test.java
+++ src/main/java/com/neoteric/jenkins/jenkindemo/Test.java
@@ -3,5 +3,6 @@ package com.neoteric.jenkins.jenkindemo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("spring project");
+        System.out.println("hii");
     }
 }
