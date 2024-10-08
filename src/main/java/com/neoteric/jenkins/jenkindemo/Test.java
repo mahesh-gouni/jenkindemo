7988e9de4d667682b9074e25721425276c64ef7d
gouni <gounimahesh9000@gmail.com>
2024-10-08 14:59:50 +0530

    dygwtu

---
+++ src/main/java/com/neoteric/jenkins/jenkindemo/Test.java
@@ -0,0 +1,7 @@
+package com.neoteric.jenkins.jenkindemo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("spring project");
+    }
+}
---
+++ src/test/java/com/neoteric/jenkins/jenkindemo/JenkindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoteric.jenkins.jenkindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
